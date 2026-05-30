0955a14517589ad0522b7f25a4db4966a31492e5
Jean Cardona <jeanpier629r@gmail.com>
2026-05-30 00:55:12 -0500

    chore: estructura final de scripts

---
+++ scripts/export_configs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "--- Iniciando exportación de configuraciones y listas ---"
+
+# 1. Exportar listas de paquetes
+echo "Generando listas de paquetes..."
+apt-mark showmanual > ~/dotfiles/apt_packages.txt
+flatpak list --app --columns=application > ~/dotfiles/flatpak_packages.txt
+snap list | awk 'NR>1 {print $1}' | grep -vE 'core|snapd|bare|gtk' > ~/dotfiles/snap_packages.txt
+
+# 2. Sincronizar carpetas de configuración
+echo "Copiando configuraciones a ~/dotfiles..."
+cp -r ~/.config/fastfetch ~/dotfiles/.config/
+cp -r ~/.config/btop ~/dotfiles/.config/
+cp -r ~/.config/zellij ~/dotfiles/.config/
+
+# 3. Empujar todo
+echo "Subiendo cambios..."
+git push origin main
